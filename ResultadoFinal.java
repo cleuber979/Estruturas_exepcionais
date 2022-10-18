374bc32969db1b84f161cc74f7e8c5dea8ffc19e
cleuber979 <105437987+cleuber979@users.noreply.github.com>
2022-10-17 23:45:15 -0300

    condição encadeada
    vazendo exercicios da dio

--- ResultadoFinal.java
+++ ResultadoFinal.java
@@ -1,9 +1,13 @@
 public class ResultadoFinal {
     public static void main(String[] args) {
-        int nota = 8;
+        int nota = 6;
 
         if (nota >= 7)
             System.out.println("Aluno aprovado");
+
+        else if (nota > 5 && nota < 7)
+            System.out.println("Aluno em recuperação");
+
         else
             System.out.println("Aluno reprovado");
 
