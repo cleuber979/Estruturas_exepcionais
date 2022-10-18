997b249019c5824fc78acffe2a739e670d40eb33
cleuber979 <105437987+cleuber979@users.noreply.github.com>
2022-10-17 22:50:05 -0300

    subindo aplicação

--- caixaEletronico.java
+++ caixaEletronico.java
@@ -3,11 +3,12 @@ public class caixaEletronico {
     public static void main(String[] args) {
 
         double saldo = 25.0;
-        double valorSolicitado = 17.0;
+        double valorSolicitado = 16.0;
 
         if (valorSolicitado < saldo)
             saldo = saldo - valorSolicitado;
         System.out.println("Olá tudo bem? Seu saldo após valor solicitado e R$ " + saldo);
 
     }
+
 }
