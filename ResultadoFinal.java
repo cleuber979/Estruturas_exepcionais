197731aa5c58c4978258cb55b73cd3a1d20046af
cleuber979 <105437987+cleuber979@users.noreply.github.com>
2022-10-17 23:23:47 -0300

    condicional simples em java
    feita depuração logica simples

---
+++ ResultadoFinal.java
@@ -0,0 +1,11 @@
+public class ResultadoFinal {
+    public static void main(String[] args) {
+        int nota = 6;
+
+        if (nota >= 7)
+            System.out.println("Aluno aprovado");
+        else
+            System.out.println("Aluno reprovado");
+
+    }
+}
