2f03f5f8b55d841de75a04f2f6ab177541ec4d72
cleuber979 <105437987+cleuber979@users.noreply.github.com>
2022-10-18 00:00:04 -0300

    expressão ternária

--- ResultadoFinal.java
+++ ResultadoFinal.java
@@ -1,15 +1,7 @@
 public class ResultadoFinal {
     public static void main(String[] args) {
-        int nota = 6;
-
-        if (nota >= 7)
-            System.out.println("Aluno aprovado");
-
-        else if (nota > 5 && nota < 7)
-            System.out.println("Aluno em recuperação");
-
-        else
-            System.out.println("Aluno reprovado");
-
+        int nota = 8;
+        String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+        System.out.println(resultado);
     }
 }
