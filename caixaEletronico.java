f6a6d6d20d7f5cc7428a4bbadbedfe5f623c5f1e
cleuber979 <105437987+cleuber979@users.noreply.github.com>
2022-10-17 23:04:14 -0300

    atualizando

--- caixaEletronico.java
+++ caixaEletronico.java
@@ -3,7 +3,7 @@ public class caixaEletronico {
     public static void main(String[] args) {
 
         double saldo = 25.0;
-        double valorSolicitado = 16.0;
+        double valorSolicitado = 15.0;
 
         if (valorSolicitado < saldo)
             saldo = saldo - valorSolicitado;
